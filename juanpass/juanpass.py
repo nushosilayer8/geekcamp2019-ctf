adc3e77b26788386998641d917294b220ffd4db7
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2019-10-19 11:22:40 +0800

    Add juanpass

---
+++ juanpass/juanpass.py
@@ -0,0 +1,10 @@
+# python3
+
+print("Do you know the flag?")
+print(">", end=" ")
+s = input()
+
+if len(s) == 23 and s[0] == 'm' and s[1] == 'i' and s[2] == 'n' and s[3] == 'i' and s[4] == 'C' and s[5] == 'T' and s[6] == 'F' and s[7] == '{' and s[8] == 'j' and s[9] == 'u' and s[10] == 'a' and s[11] == 'n' and s[12] == '_' and s[13] == 'p' and s[14] == 'u' and s[15] == 'n' and s[16] == 'c' and s[17] == 'h' and s[18] == '_' and s[19] == 'm' and s[20] == 'a' and s[21] == 'n' and s[22] == '}':
+    print("Yes you do")
+else:
+    print("No you don't")
