2023f094cccf0fe804797bdf803419177d7afd4d
Enigmatrix <enigmatrix2000@gmail.com>
2019-10-19 10:24:05 +0800

    ?

--- way_of_kings/main.c
+++ way_of_kings/main.c
@@ -9,8 +9,12 @@ struct surgebinder {
     char name[0x18];
     void* usesurge;
     int age;
-
 }
+
+void gravitation(struct surgebinder* ) {
+    puts("> %");
+}
+
 
 void print_menu() {
     puts("Menu");
