7743194737316436fe44e9b0609421fb3162bc5b
Enigmatrix <enigmatrix2000@gmail.com>
2019-10-19 10:02:04 +0800

    update soln for webserver1

---
+++ way_of_kings/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct human {
+    char name[0x20];
+    int age;
+}
+
+struct surgebinder {
+    char name[0x18];
+    void* usesurge;
+    int age;
+
+}
+
+void print_menu() {
+    puts("Menu");
+    puts("1. create human");
+}
+
+int main() {
+
+    return 0;
+}
